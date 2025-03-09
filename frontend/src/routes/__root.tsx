da3560afe4282895081689871eae060b35c3bbd9
Sabu Siyad <hello@ssiyad.com>
2025-03-09 23:36:40 +0530

    feat: header

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -5,9 +5,17 @@ export const Route = createRootRoute({
   component: () => {
     return (
       <React.Fragment>
-        <div>Hello "__root"!</div>
+        <Header />
         <Outlet />
       </React.Fragment>
     );
   },
 });
+
+const Header = () => (
+  <header className="box-border flex h-14 items-center justify-between border-b px-8">
+    <div>
+      <span className="font-semibold">Frappe Straw</span> Demo
+    </div>
+  </header>
+);
