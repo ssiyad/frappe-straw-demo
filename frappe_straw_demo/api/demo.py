8cf77302a078c214c05d141ce6de88b36bcc0e0d
Sabu Siyad <hello@ssiyad.com>
2025-03-11 01:14:13 +0530

    chore: demo api: add translations :D

--- frappe_straw_demo/api/demo.py
+++ frappe_straw_demo/api/demo.py
@@ -3,4 +3,9 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def hello_world():
-	return "Hello World"
+	return {
+		"english": "Hello, World!",
+		"malayalam": "ഹലോ വേൾഡ്!",
+		"hindi": "हैलो वर्ल्ड!",
+		"arabic": "!مرحبا بالعالم",
+	}
