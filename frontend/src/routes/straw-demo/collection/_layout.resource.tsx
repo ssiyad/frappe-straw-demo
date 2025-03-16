aac39bff6e83277574f79f9a78e000d833144a3b
Sabu Siyad <hello@ssiyad.com>
2025-03-16 23:56:58 +0530

    chore: resource: get rid of form

--- frontend/src/routes/straw-demo/collection/_layout.resource.tsx
+++ frontend/src/routes/straw-demo/collection/_layout.resource.tsx
@@ -1,24 +1,14 @@
 import { Sandbox } from '@/components/features/sandbox';
 import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
-import { JsonCompatible } from '@/types/json';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { type JsonCompatible } from '@/types/json';
 import { createFileRoute } from '@tanstack/react-router';
 import { useDebounce } from '@uidotdev/usehooks';
 import { useResource } from 'frappe-straw';
 import { Loader2Icon } from 'lucide-react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { useState } from 'react';
 
 export const Route = createFileRoute('/straw-demo/collection/_layout/resource')(
   {
@@ -31,19 +21,13 @@ export const Route = createFileRoute('/straw-demo/collection/_layout/resource')(
 );
 
 function RouteComponent() {
-  const formSchema = z.object({
-    url: z.string().nonempty(),
-  });
+  const [url, setUrl] = useState(
+    'https://jsonplaceholder.typicode.com/todos/1',
+  );
 
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      url: 'https://jsonplaceholder.typicode.com/todos/1',
-    },
-  });
-
-  const url = useDebounce(form.watch('url'), 500);
-  const { data, error, loading, refresh } = useResource<JsonCompatible>(url);
+  const { data, error, loading, refresh } = useResource<JsonCompatible>(
+    useDebounce(url, 300),
+  );
 
   const exampleUrls = [
     'frappe_straw_demo.api.demo.hello_world',
@@ -59,92 +43,38 @@ function RouteComponent() {
       response={data}
       error={error}
     >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(refresh)} className="w-96 space-y-4">
-          <FormField
-            control={form.control}
-            name="url"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>URL</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormDescription>
-                  Url can be any Frappe method, internal Url, or external Url.
-                  Try one of the following:
-                </FormDescription>
-                <ul className="my-2 cursor-pointer list-disc space-y-2 text-sm text-neutral-800">
-                  {exampleUrls.map((url) => (
-                    <li key={url}>
-                      <a onClick={() => field.onChange(url)}>{url}</a>
-                    </li>
-                  ))}
-                </ul>
-                <FormMessage />
-              </FormItem>
-            )}
+      <div className="space-y-4 text-sm">
+        <Label>URL</Label>
+        <Input value={url} onChange={(event) => setUrl(event.target.value)} />
+        <div>
+          Url can be any Frappe method, internal Url, or external Url. Try one
+          of the following:
+        </div>
+        <div>
+          <ul className="my-2 cursor-pointer list-disc space-y-2 text-sm text-neutral-800">
+            {exampleUrls.map((url) => (
+              <li key={url}>
+                <a onClick={() => setUrl(url)}>{url}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Button onClick={() => refresh()} disabled={loading} className="w-full">
+          <Loader2Icon
+            className={cn('hidden', {
+              'block animate-spin': loading,
+            })}
           />
-          <Button onClick={refresh} disabled={loading} className="w-full">
-            <Loader2Icon
-              className={cn('hidden', {
-                'block animate-spin': loading,
-              })}
-            />
-            Refresh
-          </Button>
-        </form>
-      </Form>
+          Refresh
+        </Button>
+      </div>
     </Sandbox>
   );
 }
 
-const exampleCode = `import { zodResolver } from '@hookform/resolvers/zod';
-import { createFileRoute } from '@tanstack/react-router';
-import { useResource } from 'frappe-straw';
-import { Loader2Icon } from 'lucide-react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+const exampleCode = `import { useResource } from 'frappe-straw';
+import { type JsonCompatible } from '@/types/json';
 
-const formSchema = z.object({
-  url: z.string().nonempty(),
-});
-
-const form = useForm({
-  resolver: zodResolver(formSchema),
-  defaultValues: {
-    url: 'https://jsonplaceholder.typicode.com/todos/1',
-  },
-});
-
-const { data, error, loading, refresh } = useResource<JsonCompatible>(
-  form.watch('url'),
-);
-
-return (
-  <Form {...form}>
-    <form onSubmit={form.handleSubmit(refresh)} className="w-96 space-y-4">
-      <FormField
-        control={form.control}
-        name="url"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>URL</FormLabel>
-            <FormControl>
-              <Input {...field} />
-            </FormControl>
-          </FormItem>
-        )}
-      />
-      <Button type='submit' disabled={loading}>
-        <Loader2Icon
-          className={cn('hidden', {
-            'block animate-spin': loading,
-          })}
-        />
-        Refresh
-      </Button>
-    </form>
-  </Form>
-)
+const url = 'https://jsonplaceholder.typicode.com/todos/3';
+const { data, error, loading, refresh } = useResource<JsonCompatible>(url);
 `;
